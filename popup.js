3805bc9d1974582a5f724cc8151377114675bb7e
cadeelliott <137833718+cadeelliott@users.noreply.github.com>
2023-11-22 15:12:59 -0600

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,17 @@
+function getRandomInterval() {
+  return Math.floor(Math.random() * (60000 - 10000 + 1) + 10000); // Random interval between 10s and 60s
+}
+
+function showPopup() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "showPopup" });
+  });
+}
+
+function setRandomInterval() {
+  setInterval(function() {
+    showPopup();
+  }, getRandomInterval());
+}
+
+document.addEventListener("DOMContentLoaded", setRandomInterval);
