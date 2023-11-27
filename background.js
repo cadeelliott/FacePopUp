c33d5c807e901a7b48e255ce227e50a83d7a7dc4
cadeelliott <137833718+cadeelliott@users.noreply.github.com>
2023-11-27 12:05:12 -0600

    Update background.js
    Error fix 2

--- background.js
+++ background.js
@@ -23,6 +23,28 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     setRandomInterval();
   }
 });
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.action.setBadgeText({ text: "ON" });
+});
 
-// Set the random interval when the extension is installed or updated
-setRandomInterval();
+function getRandomInterval() {
+  return Math.floor(Math.random() * (60000 - 10000 + 1) + 10000); // Random interval between 10s and 60s
+}
+
+function showPopup() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "showPopup" });
+  });
+}
+
+function setRandomInterval() {
+  setInterval(function() {
+    showPopup();
+  }, getRandomInterval());
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === "setRandomInterval") {
+    setRandomInterval();
+  }
+});
