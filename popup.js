e4be6aa7b80e892b5e4878ad3000b3bf7126fc7a
cadeelliott <137833718+cadeelliott@users.noreply.github.com>
2023-11-27 11:59:20 -0600

    Update popup.js
    Error fix

--- popup.js
+++ popup.js
@@ -1,19 +1,3 @@
-function getRandomInterval() {
-  return Math.floor(Math.random() * (60000 - 10000 + 1) + 10000); // Random interval between 10s and 60s
-}
-
-function showPopup() {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { action: "showPopup" });
-  });
-}
-
-function setRandomInterval() {
-  setInterval(function() {
-    showPopup();
-  }, getRandomInterval());
-}
-
-chrome.runtime.onInstalled.addListener(function() {
-  setRandomInterval();
+document.addEventListener("DOMContentLoaded", function() {
+  chrome.runtime.sendMessage({ action: "setRandomInterval" });
 });
